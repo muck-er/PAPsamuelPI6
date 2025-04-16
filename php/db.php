eb38f5877b9f73273cc8b8bf36d0f6dc5f6f04c3
muck-er <stemspam530@gmail.com>
2025-04-16 10:13:04 +0100

    Update db.php

--- php/db.php
+++ php/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "honoriscausafit";
+$dbname = "othrysfit";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
